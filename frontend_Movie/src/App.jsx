31ab9c04b0da64eb3c074c3204c547d5fe52d77e
Priya2247 <priyasekar2247@gmail.com>
2025-04-14 18:26:15 +0530

    feat: inserted a moviecard component

--- frontend_Movie/src/App.jsx
+++ frontend_Movie/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Moviecard from "./components/MovieCard"
 
 function App() {
+  const date="2025"
  return(
   <>
-  <div>
-    <p>Hello Everyone</p>
-  </div>
-  <div>
-    <p>That's cool</p>
-  </div>
-  </>
+  {date==="2025"?
+ <Moviecard movie={{title:"Tim's film",release_date:"2025"}}/>:
+ <Moviecard movie={{title:"Pushpa",release_date:"2024"}}/>
+  }
+ </>
  )
  
 }
